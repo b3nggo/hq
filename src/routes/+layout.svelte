3b6764e527fab2720e223dac727a7c0239492e60
Irhas <muhammadirhasalh@gmail.com>
2025-03-01 11:18:30 +0700

    chore: init SvelteKit (#1)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { children } = $props()
+  import '../app.css'
+</script>
+
+{@render children()}
+
+<style>
+  @import 'https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css';
+</style>
